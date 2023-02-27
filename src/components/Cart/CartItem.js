f0c9cac3614f91ff13190ea1fbee7197cb41e2b5
voyager <cdspacenoob@gmail.com>
2023-02-27 15:47:08 +0900

    add and remove item Logic

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,27 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
+import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
-  const { title, quantity, totalPrice, price } = props.item;
+  const { title, quantity, totalPrice, price, id } = props.item;
+
+  const dispatch = useDispatch();
+
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id: id,
+        title: title,
+        quantity: quantity,
+        totalPrice: totalPrice,
+        price: price,
+      })
+    );
+  };
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +37,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
