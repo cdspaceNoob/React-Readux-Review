d089e7975539028fb4aa54d81e4066268c4d9eb7
voyager <cdspacenoob@gmail.com>
2023-02-28 16:20:03 +0900

    keep code lean by using actionCreator

--- src/App-reducer.js
+++ src/App-reducer.js
@@ -5,7 +5,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/Notification/Notification";
-import { sendCartData } from "./store/cart-slice";
+import { uiActions } from "./store/ui-slice";
 
 let isInitial = true;
 
@@ -16,12 +16,49 @@ const App = () => {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data",
+        })
+      );
+      const response = await fetch(
+        "https://react-hooks-fe144-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data fail.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Send cart data successfully,",
+        })
+      );
+    };
+
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    dispatch(sendCartData(cart));
+    sendCartData().catch((error) =>
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "error",
+          message: "Sending cart data failed.",
+        })
+      )
+    );
   }, [cart, dispatch]);
 
   return (
